ef5463b839592c47db8439a7b6d15aac4880492c
Vadim Ogievetsky <vadimon@gmail.com>
2015-09-15 16:29:34 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,299 @@
+# plyql
+
+A SQL parser for plywood
+
+## Installation
+
+PlyQL is built on top of node so make sure you have node >= 0.12 installed.
+
+```
+npm install -g plyql
+```
+
+The global install will make the `plyql` command available system wide.
+
+## Usage
+
+Currently only queries to Druid are supported. More support will come in the future. 
+
+The CLI supports the following options:
+
+Option                     | Description
+---------------------------|-----------------------------------------
+`--help`                   | print this help message
+`--version`                | display the version number
+`-v`, `--verbose`          | display the queries that are being made
+`-h`, `--host`             | the host to connect to
+`-d`, `--data-source`      | use this data source for the query (supersedes FROM clause)
+`-i`, `--interval`         | add (AND) a `__time` filter between NOW-INTERVAL and NOW
+`-q`, `--query`            | the query to run
+`-o`, `--output`           | specify the output format. Possible values: `json` **(default)**, `csv`, `tsv`, `flat`
+`-a`, `--allow`            | enable a behaviour that is turned off by default `eternity` allow queries not filtered on time `select` allow select queries
+`-r`, `--retry`            | the number of tries a query should be attempted on error, 0 = unlimited, (default: 2)
+`-c`, `--concurrent`       | the limit of concurrent queries that could be made simultaneously, 0 = unlimited, (default: 2)
+`-fu`, `--force-unique`    | force a column to be interpreted as a hyperLogLog uniques
+`-fh`, `--force-histogram` | force a column to be interpreted as an approximate histogram
+
+## Examples
+
+Say your Druid broker node is located at `10.20.30.40` on the standard port (`8080`) and that there is a
+datasource called `twitterstream` that has tweets in it.
+
+Here is a simple query that gets the maximum of the `__time` column which tells you what how up to date the data in the database is.
+
+```
+plyql -h 10.20.30.40 -q "SELECT MAX(__time) AS maxTime FROM twitterstream"
+```
+
+Returns:
+
+```json
+[
+  {
+    "maxTime": {
+      "type": "TIME",
+      "value": "2015-04-16T20:49:00.000Z"
+    }
+  }
+]
+```
+
+Ok now you might want to examine the different hashtags that are trending.
+
+You might do a group by on the `first_hashtag` column like this:
+
+```
+plyql -h 10.20.30.40 -q "
+SELECT
+first_hashtag as hashtag,
+COUNT() as cnt
+FROM twitterstream
+WHERE '2015-04-15T00:00:00' <= __time AND __time < '2015-04-16T00:00:00'
+GROUP BY first_hashtag
+ORDER BY cnt DESC
+LIMIT 5
+"
+```
+
+This will throw an error because there is no time filter specified and the plyql guards against this.
+
+This behaviour can be disabled using the `--allow eternity` flag but it is generally bad idea to do it when working with
+large amounts of data as it can issue computationally prohibitive queries.
+  
+Try it again, with a time filter:
+  
+```
+plyql -h 10.20.30.40 -q "
+SELECT
+first_hashtag as hashtag,
+COUNT() as cnt
+FROM twitterstream
+WHERE '2015-04-15T00:00:00' <= __time AND __time < '2015-04-16T00:00:00'
+GROUP BY first_hashtag
+ORDER BY cnt DESC
+LIMIT 5
+"
+```
+
+Results:
+  
+```json
+[
+  {
+    "cnt": 3480496,
+    "hashtag": "No Hashtag"
+  },
+  {
+    "cnt": 7691,
+    "hashtag": "FOLLOWTRICK"
+  },
+  {
+    "cnt": 6059,
+    "hashtag": "5YearsSinceNiallsAudition"
+  },
+  {
+    "cnt": 4392,
+    "hashtag": "ShotsUpdateNextWeek"
+  },
+  {
+    "cnt": 3729,
+    "hashtag": "EXO"
+  }
+]
+```
+  
+The plyql has an option `--interval` (`-i`) that automatically filters time on the last `interval` worth of time.
+It is useful if you do not want to type out a time filter.
+
+```
+plyql -h 10.20.30.40 -i P1D -q "
+SELECT
+first_hashtag as hashtag,
+COUNT() as cnt
+FROM twitterstream
+GROUP BY first_hashtag
+ORDER BY cnt DESC
+LIMIT 5
+"
+```
+
+To get a breakdown by time the `TIME_BUCKET` function can be used:
+
+```
+plyql -h 10.20.30.40 -i P1D -q "
+SELECT
+SUM(tweet_length) as TotalTweetLength
+FROM twitterstream
+GROUP BY TIME_BUCKET(__time, PT1H, 'Etc/UTC')
+"
+```
+
+Returns:
+
+```json
+[
+  {
+    "TotalTweetLength": 9748287,
+    "split": {
+      "start": "2015-04-18T15:00:00.000Z",
+      "end": "2015-04-18T16:00:00.000Z",
+      "type": "TIME_RANGE"
+    }
+  },
+  {
+    "TotalTweetLength": 15321726,
+    "split": {
+      "start": "2015-04-18T16:00:00.000Z",
+      "end": "2015-04-18T17:00:00.000Z",
+      "type": "TIME_RANGE"
+    }
+  },
+  "... results omitted ..."
+]
+```
+
+Note that the grouping column was not selected but was still returned as if `TIME_BUCKET(__time, PT1H, 'Etc/UTC') as 'split'`
+was one of the select clauses.
+
+Time parting is also supported, here is an example:
+
+```
+plyql -h 10.20.30.40 -i P1W -q "
+SELECT
+TIME_PART(__time, HOUR_OF_DAY, 'Etc/UTC') as HourOfDay,
+SUM(tweet_length) as TotalTweetLength
+FROM twitterstream
+GROUP BY TIME_PART(__time, HOUR_OF_DAY, 'Etc/UTC')
+ORDER BY TotalTweetLength DESC
+LIMIT 3
+"
+```
+
+Returns:
+
+```json
+[
+  {
+    "HourOfDay": 14,
+    "TotalTweetLength": 115983429
+  },
+  {
+    "HourOfDay": 13,
+    "TotalTweetLength": 111398041
+  },
+  {
+    "HourOfDay": 15,
+    "TotalTweetLength": 106998577
+  }
+]
+```
+
+Here is an advanced example that gets the top 5 hashtags by time. PlyQL allows us to nest queries as
+aggregates like so:
+
+```
+plyql -h 10.20.30.40 -i P1D -q "
+SELECT
+first_hashtag as hashtag,
+COUNT() as cnt,
+(
+  SELECT
+  SUM(tweet_length) as TotalTweetLength
+  GROUP BY TIME_BUCKET(__time, PT1H, 'Etc/UTC')
+  LIMIT 3    -- only get the first 3 hours to keep this example output small
+) as 'ByTime'
+FROM twitterstream
+GROUP BY first_hashtag
+ORDER BY cnt DESC
+LIMIT 5
+"
+```
+
+Returns:
+
+```json
+[
+  {
+    "hashtag": "No Hashtag",
+    "cnt": 3628171,
+    "ByTime": [
+      {
+        "TotalTweetLength": 658517,
+        "split": {
+          "start": "2015-04-19T06:00:00.000Z",
+          "end": "2015-04-19T07:00:00.000Z",
+          "type": "TIME_RANGE"
+        }
+      },
+      {
+        "TotalTweetLength": 8218030,
+        "split": {
+          "start": "2015-04-19T07:00:00.000Z",
+          "end": "2015-04-19T08:00:00.000Z",
+          "type": "TIME_RANGE"
+        }
+      },
+      {
+        "TotalTweetLength": 8480978,
+        "split": {
+          "start": "2015-04-19T08:00:00.000Z",
+          "end": "2015-04-19T09:00:00.000Z",
+          "type": "TIME_RANGE"
+        }
+      }
+    ]
+  },
+  {
+    "hashtag": "FOLLOWTRICK",
+    "cnt": 11553,
+    "ByTime": [
+      {
+        "TotalTweetLength": 5227,
+        "split": {
+          "start": "2015-04-19T06:00:00.000Z",
+          "end": "2015-04-19T07:00:00.000Z",
+          "type": "TIME_RANGE"
+        }
+      },
+      "... results omitted ..."
+    ]
+  },
+  "... results omitted ..."
+]
+```
+
+## Roadmap
+
+Here is a list of features that is not currently supported that are in the works:
+
+* Different outputs like CSV, TSV, etc.
+* Query simulation - preview the queries that will be run without running them
+* Expressions within aggregate functions - `SUM(price + tax)`
+* Sub-queries in WHERE clauses  
+* JOIN support
+* DESCRIBE queries to introspect tables
+* Window functions
+
+## Questions & Support
+
+Please direct all questions to our [user groups](https://groups.google.com/forum/#!forum/imply-user-group).
---
+++ src/cli.ts
@@ -0,0 +1,330 @@
+/// <reference path="../typings/node/node.d.ts" />
+/// <reference path="../typings/nopt/nopt.d.ts" />
+/// <reference path="../node_modules/plywood/build/plywood.d.ts" />
+/// <reference path="../node_modules/plywood-druid-requester/build/plywood-druid-requester.d.ts" />
+"use strict";
+
+import fs = require('fs');
+import path = require("path");
+import Q = require('q');
+import nopt = require("nopt");
+
+import { WallTime, Timezone, Duration } from "chronoshift";
+if (!WallTime.rules) {
+  var tzData = require("chronoshift/lib/walltime/walltime-data.js");
+  WallTime.init(tzData.rules, tzData.zones);
+}
+
+import { $, Expression, RefExpression, ChainExpression, Datum, Dataset, TimeRange, External, ApplyAction, AttributeJSs, helper, version } from "plywood";
+import { druidRequesterFactory } from 'plywood-druid-requester';
+
+function printUsage() {
+  console.log(`
+Usage: plyql [options]
+
+Example: plyql -h 10.20.30.40 -q "SELECT MAX(__time) AS maxTime FROM twitterstream"
+
+       --help         print this help message
+       --version      display the version number
+  -v,  --verbose      display the queries that are being made
+  -h,  --host         the host to connect to
+  -d,  --data-source  use this data source for the query (supersedes FROM clause)
+  -i,  --interval     add (AND) a __time filter between NOW-INTERVAL and NOW
+  -q,  --query        the query to run
+  -o,  --output       the output format. Possible values: json (default), csv, tsv, flat
+  -r,  --retry        the number of tries a query should be attempted on error, 0 = unlimited, (default: 2)
+  -c,  --concurrent   the limit of concurrent queries that could be made simultaneously, 0 = unlimited, (default: 2)
+
+  -fu, --force-unique     force a column to be interpreted as a hyperLogLog uniques
+  -fh, --force-histogram  force a column to be interpreted as an approximate histogram
+
+  -a,  --allow        enable a behaviour that is turned off by default
+           eternity     allow queries not filtered on time
+           select       allow select queries
+`
+  )
+}
+
+function printVersion(): void {
+  var cliPackageFilename = path.join(__dirname, '..', 'package.json');
+  try {
+    var cliPackage = JSON.parse(fs.readFileSync(cliPackageFilename, 'utf8'));
+  } catch (e) {
+    console.log("could not read cli package", e.message);
+    return;
+  }
+  console.log(`plyql version ${cliPackage.version} [beta] (plywood version ${version})`);
+}
+
+function parseIntervalString(str: string): TimeRange {
+  var parts = str.split('/');
+  if (parts.length > 2) throw new Error(`Can not parse string ${str}`);
+  var p0: string = parts[0];
+  var p1: string = parts.length === 2 ? parts[1] : (new Date()).toISOString();
+
+  var start: Date = null;
+  var end: Date = null;
+  var duration: Duration = null;
+  if (p0[0] === 'P') {
+    duration = Duration.fromJS(p0);
+    end = new Date(p1);
+    start = duration.move(end, Timezone.UTC, -1);
+  } else if (p1[0] === 'P') {
+    start = new Date(p0);
+    duration = Duration.fromJS(p1);
+    end = duration.move(end, Timezone.UTC, 1);
+  } else {
+    start = new Date(p0);
+    end = new Date(p1);
+  }
+
+  return TimeRange.fromJS({ start, end });
+}
+
+function getDatasourceName(ex: Expression): string {
+  var name: string = null;
+  ex.some(ex => {
+    if (ex instanceof ChainExpression) {
+      var expression = ex.expression;
+      var firstAction = ex.actions[0];
+      if (expression instanceof RefExpression) {
+        name = expression.name;
+        return true;
+      } else if (firstAction instanceof ApplyAction && (<ApplyAction>firstAction).name === 'data') {
+        var firstActionExpression = firstAction.expression;
+        if (firstActionExpression instanceof RefExpression) {
+          name = firstActionExpression.name;
+          return true;
+        }
+      }
+    }
+    return null;
+  });
+  return name;
+}
+
+function parseArgs() {
+  return nopt(
+    {
+      "host": String,
+      "data-source": String,
+      "help": Boolean,
+      "query": String,
+      "interval": String,
+      "version": Boolean,
+      "verbose": Boolean,
+      "retry": Number,
+      "concurrent": Number,
+      "output": String,
+      "allow": [String, Array],
+      "force-unique": [String, Array],
+      "force-histogram": [String, Array]
+    },
+    {
+      "h": ["--host"],
+      "q": ["--query"],
+      "v": ["--verbose"],
+      "d": ["--data-source"],
+      "i": ["--interval"],
+      "a": ["--allow"],
+      "r": ["--retry"],
+      "c": ["--concurrent"],
+      "o": ["--output"],
+      "fu": ["--force-unique"],
+      "fh": ["--force-histogram"]
+    },
+    process.argv
+  );
+}
+
+export function run() {
+  var parsed = parseArgs();
+
+  if (parsed.argv.original.length === 0 || parsed['help']) {
+    printUsage();
+    return;
+  }
+
+  if (parsed['version']) {
+    printVersion();
+    return;
+  }
+
+  var verbose: boolean = parsed['verbose'];
+
+  // Get allow
+  var allows: string[] = parsed['allow'] || [];
+  for (let allow of allows) {
+    if (!(allow === 'eternity' || allow === 'select')) {
+      console.log("Unexpected allow", allow);
+      return;
+    }
+  }
+
+  // Get forced attribute overrides
+  var attributeOverrides: AttributeJSs = {};
+  var forceUnique: string[] = parsed['force-unique'] || [];
+  for (let attributeName of forceUnique) {
+    attributeOverrides[attributeName] = { special: 'unique' };
+  }
+  var forceHistogram: string[] = parsed['force-histogram'] || [];
+  for (let attributeName of forceHistogram) {
+    attributeOverrides[attributeName] = { special: 'histogram' };
+  }
+
+  // Get output
+  var output: string = (parsed['output'] || 'json').toLowerCase();
+  if (output !== 'json' && output !== 'csv' && output !== 'tsv' && output !== 'flat') {
+    console.log(`output must be one of json, csv, tsv, or flat (is ${output}})`);
+    return;
+  }
+
+  // Get host
+  var host: string = parsed['host'];
+  if (!host) {
+    console.log("must have host (for now)");
+    return;
+  }
+
+  // Get SQL
+  var query: string = parsed['query'];
+  var expression: Expression = null;
+  if (query) {
+    query = query.trim();
+    if (/^SELECT/i.test(query)) {
+      try {
+        expression = Expression.parseSQL(query);
+      } catch (e) {
+        console.log("Could not parse query as SQL:", e.message);
+        return;
+      }
+
+    } else if (query[0] === '$') {
+      try {
+        expression = Expression.parse(query);
+      } catch (e) {
+        console.log("Could not parse query as plywood:", e.message);
+        return;
+      }
+
+    } else if (query[0] === '{') {
+      try {
+        expression = Expression.fromJS(JSON.parse(query));
+      } catch (e) {
+        console.log("Could not parse query as plywood:", e.message);
+        return;
+      }
+
+    } else {
+      console.log("Could not determine query type (query should start with 'SELECT', '$', or '{')");
+      return;
+    }
+  } else {
+    console.log("no query found please use --query (-q) flag");
+    return;
+  }
+
+  if (verbose) {
+    console.log('Parsed query as the following plywood expression (as JSON):');
+    console.log(JSON.stringify(expression, null, 2));
+    console.log('---------------------------');
+  }
+
+  var dataSource = getDatasourceName(expression);
+  if (!dataSource) {
+    console.log("must have data source");
+    return;
+  }
+
+  var requester: Requester.PlywoodRequester<any>;
+  requester = druidRequesterFactory({
+    host: host,
+    timeout: 30000
+  });
+
+  var retry: number = parsed.hasOwnProperty('retry') ? parsed['retry'] : 2;
+  if (retry > 0) {
+    requester = helper.retryRequesterFactory({
+      requester: requester,
+      retry: retry,
+      delay: 500,
+      retryOnTimeout: false
+    });
+  }
+
+  if (verbose) {
+    requester = helper.verboseRequesterFactory({
+      requester: requester
+    });
+  }
+
+  var concurrent: number = parsed.hasOwnProperty('concurrent') ? parsed['concurrent'] : 2;
+  if (concurrent > 0) {
+    requester = helper.concurrentLimitRequesterFactory({
+      requester: requester,
+      concurrentLimit: concurrent
+    });
+  }
+
+  var timeAttribute = '__time';
+
+  var filter: Expression = null;
+  var intervalString: string = parsed['interval'];
+  if (intervalString) {
+    try {
+      var interval = parseIntervalString(intervalString);
+    } catch (e) {
+      console.log("Could not parse interval", intervalString);
+      console.log(e.message);
+      return;
+    }
+
+    filter = $(timeAttribute).in(interval);
+  }
+
+  var dataset = External.fromJS({
+    engine: 'druid',
+    dataSource,
+    timeAttribute,
+    allowEternity: allows.indexOf('eternity') !== -1,
+    allowSelectQueries: allows.indexOf('select') !== -1,
+    filter,
+    requester,
+    attributeOverrides
+  });
+
+  var context: Datum = {};
+  context[dataSource] = dataset;
+
+  expression.compute(context)
+    .then(
+      (data: Dataset) => {
+        var outputStr: string;
+        switch (output) {
+          case 'json':
+            outputStr = JSON.stringify(data, null, 2);
+            break;
+
+          case 'csv':
+            outputStr = data.toCSV();
+            break;
+
+          case 'tsv':
+            outputStr = data.toTSV();
+            break;
+
+          case 'flat':
+            outputStr = JSON.stringify(data.flatten(), null, 2);
+            break;
+
+          default:
+            outputStr = 'Unknown output type';
+            break;
+        }
+        console.log(outputStr);
+      },
+      (err: Error) => {
+        console.log(`There was an error getting the data: ${err.message}`);
+      }
+    ).done()
+}
